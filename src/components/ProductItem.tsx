7fdb2157f7518830a723ccef8187c39227afb310
Jens Kristian Espevik <jenskristian@jenske.net>
2024-02-28 15:17:23 +0100

    update CSS

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,12 +2,14 @@ import { Product } from "../util/interface";
 
 const ProductItem = ({ product }: { product: Product; }) => {
   return (
-    <div className="productItem">
-      <div className="productImage"><img src={`src/assets/nectars/${product.name}.webp`} alt={product.name} /></div>
-      <h2>{product.name}</h2>
-      <p>{product.description}</p>
-      <p>Price: {product.price}</p>
-      <p>In Stock: {product.in_stock ? 'Yes' : 'No'}</p>
+    <div className="productItem" style={{ backgroundImage: `url("src/assets/nectars/${product.name}.webp")` }}>
+      {/* <div className="productImage"><img src={`src/assets/nectars/${product.name}.webp`} alt={product.name} /></div> */}
+      <div className="productInfo">
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <p>Price: {product.price}</p>
+        <p>In Stock: {product.in_stock ? 'Yes' : 'No'}</p>
+      </div>
     </div>
   );
 };
